b2c97ac35525bc1405576ffa8a7cec089c966587
Brandon Courtney <brandon.h.courtney@gmail.com>
2016-12-08 22:45:22 -0800

    Chat component renders

---
+++ client/js/Chat.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Chat extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div id="chat">
+        this is the Chat Component
+      </div>
+    )
+  }
+};
+
+export default Chat;
--- client/js/Main.js
+++ client/js/Main.js
@@ -1,15 +1,34 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import Chat from './Chat.js';
+import axios from 'axios';
 
 class Main extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      messages: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('../fixtures/fakedata.json')
+      .then(function (res) {
+        return res.data.messages;
+      })
+      .then((messages) => {
+        this.setState({messages, messages})
+        return messages;
+      })
+      .catch(function (error) {
+        return console.log(error);
+      });
   }
 
   render() {
     return (
-      <div>
-        React is working!
+      <div id="container" className="center-block col-lg-4">
+        <Chat />
       </div>
     )
   }
